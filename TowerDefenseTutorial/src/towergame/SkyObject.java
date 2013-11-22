5b6bfed8b98d2cfbd4fbe476b9a5dca546a10d89
eettlin <mrumrocks@gmail.com>
2013-11-22 10:33:26 -0800

    Sky time of day changer

--- TowerDefenseTutorial/src/towergame/SkyObject.java
+++ TowerDefenseTutorial/src/towergame/SkyObject.java
@@ -5,15 +5,46 @@ import java.awt.Graphics2D;
 
 import jgame.Context;
 import jgame.GObject;
-import jgame.listener.DelayListener;
 import jgame.listener.FrameListener;
+import jgame.listener.TimerListener;
 
 public class SkyObject extends GObject {
 
-	float nightSky = (float) 0.80;
-	
+	private static final float DAY_SKY = 0.8f;
+	private static final float NIGHT_SKY = 0.5f;
+	float skyValue = (float) 0.80;
+
 	public SkyObject() {
 		this.setSize(90, 15);
+		TimerListener dl = new TimerListener(72) {
+			boolean b = false;
+
+			@Override
+			public void invoke(GObject target, Context context) {
+				b = !b;
+				switchTimeOfDay(b);
+			}
+		};
+		addListener(dl);
+	}
+
+	public void switchTimeOfDay(final boolean isNight) {
+		final float skyChange = isNight ? -0.01f : 0.01f;
+		final float start = isNight ? DAY_SKY : NIGHT_SKY;
+		final float end = isNight ? NIGHT_SKY : DAY_SKY;
+
+		skyValue = start;
+
+		FrameListener fl = new FrameListener() {
+			@Override
+			public void invoke(GObject target, Context context) {
+				skyValue += skyChange;
+				if (isNight ? (skyValue <= end) : (skyValue >= end)) {
+					target.removeListener(this);
+				}
+			}
+		};
+		addListener(fl);
 	}
 
 	@Override
@@ -21,27 +52,16 @@ public class SkyObject extends GObject {
 		super.paint(g);
 
 		g.fillRect(0, 0, getIntWidth(), getIntHeight());
-		
-		DelayListener dl = new DelayListener(200) {
-			
-			@Override
-			public void invoke(GObject target, Context context) {
-				nightSky = (float) 0.50;				
-			}
-		};
-		addListener(dl);
-		/*FrameListener fl = new FrameListener() {
-			@Override
-			public void invoke(GObject target, Context context) {
-				skyHue += 0.000005;
-			}
-		};
-		addListener(fl);*/
-		g.setColor(Color.getHSBColor( nightSky, nightSky, .67f));
-		g.fillRect(45, 5,900,700);
-		
-		
-//		/System.out.println(skyHue);
+
+		/*
+		 * FrameListener fl = new FrameListener() {
+		 * 
+		 * @Override public void invoke(GObject target, Context context) {
+		 * skyHue += 0.000005; } }; addListener(fl);
+		 */
+		g.setColor(Color.getHSBColor(skyValue, skyValue, .67f));
+		g.fillRect(45, 5, 900, 700);
+
+		// /System.out.println(skyHue);
 	}
 }
-
